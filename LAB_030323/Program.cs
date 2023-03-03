5da59ac595141426bbd6613223c79c40a4d68335
Velmeri <danielmikhh2gmail.com>
2023-03-03 12:28:15 +0100

    corrected \\ on \ in the e -mail template

--- LAB_030323/Program.cs
+++ LAB_030323/Program.cs
@@ -13,7 +13,7 @@ namespace LAB_030323
         {
             string InPut = Console.ReadLine();
 
-            if (Regex.IsMatch(InPut, @"^[A-Z,a-z,0-9]+@[A-Z,a-z,0-9]+\\.com$"))
+            if (Regex.IsMatch(InPut, @"^[A-Z,a-z,0-9]+@[A-Z,a-z,0-9]+\.com$"))
                 Console.WriteLine("This is e-mail.");
 
             else if (Regex.IsMatch(InPut, @"^(\+\d{1,2})(\s|.)(\d{3})(\s|.)(\d{3})(\s|.)(\d{4})$"))
