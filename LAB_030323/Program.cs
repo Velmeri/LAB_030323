d995e101f9f70a154f976546dfea884375d852a8
Velmeri <danielmikhh2gmail.com>
2023-03-03 12:25:07 +0100

    Added $ to the end of the pattern of the Ukrainian phone number

--- LAB_030323/Program.cs
+++ LAB_030323/Program.cs
@@ -16,7 +16,7 @@ namespace LAB_030323
             if (Regex.IsMatch(InPut, @"^[A-Z,a-z,0-9]+@[A-Z,a-z,0-9]+\\.com$"))
                 Console.WriteLine("This is e-mail.");
 
-            else if (Regex.IsMatch(InPut, @"^(\+\d{1,2})(\s|.)(\d{3})(\s|.)(\d{3})(\s|.)(\d{4})"))
+            else if (Regex.IsMatch(InPut, @"^(\+\d{1,2})(\s|.)(\d{3})(\s|.)(\d{3})(\s|.)(\d{4})$"))
                 Console.WriteLine("This is mobile number.");
 
             else Console.WriteLine("I don't know what is it.");
